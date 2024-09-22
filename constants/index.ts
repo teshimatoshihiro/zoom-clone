f78d61d163e0ac4b7c81b3706c261bff48eaa75a
teshimatoshihiro <t.teshima.6.21@gmail.com>
2024-09-22 13:08:33 +0900

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+    {
+        imgURL: '/icons/Home.svg',
+        route: '/',
+        label: 'ホーム',
+    },
+
+    {
+        imgURL: '/icons/upcoming.svg',
+        route: '/upcoming',
+        label: '最新',
+    },
+    {
+        imgURL: '/icons/previous.svg',
+        route: '/previous',
+        label: '以前の',
+    },
+    {
+        imgURL: '/icons/Video.svg',
+        route: '/recordings',
+        label: '録画',
+    },
+    {
+        imgURL: '/icons/add-personal.svg',
+        route: '/personal-room',
+        label: 'パーソナルルーム',
+    },
+];
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+];
